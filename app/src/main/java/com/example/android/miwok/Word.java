9390a3747dd5e192da3664193f6379903a4393d7
Kevin Chung <chungkev7@gmail.com>
2020-02-06 10:54:02 -0500

    Added tabs to viewpager and additional styling

--- app/src/main/java/com/example/android/miwok/Word.java
+++ app/src/main/java/com/example/android/miwok/Word.java
@@ -2,18 +2,14 @@ package com.example.android.miwok;
 
 public class Word {
 
-    // Default translation for the word
-    private String mDefaultTranslation;
-
-    // Miwok translation for the word
-    private String mMiwokTranslation;
-
-    // Image resource ID for the word
-    private int mImageResourceId = NO_IMAGE_PROVIDED;
-
     // Constant value that represents no image was provided for this word
     private static final int NO_IMAGE_PROVIDED = -1;
-
+    // Default translation for the word
+    private String mDefaultTranslation;
+    // Miwok translation for the word
+    private String mMiwokTranslation;
+    // Image resource ID for the word
+    private int mImageResourceId = NO_IMAGE_PROVIDED;
     // Audio resource ID for the word
     private int mAudioResourceId;
 
